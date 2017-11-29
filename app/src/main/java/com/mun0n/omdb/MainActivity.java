9d785f27204eba73fe68cacd0e457aae9f9ce7b0
Javi Muñoz <munozmunozjavier@gmail.com>
2017-11-29 10:37:46 +0100

    Initial commit

---
+++ app/src/main/java/com/mun0n/omdb/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mun0n.omdb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
