1787bf9db9d271702339554d41e1bbff4e9530ce
Javi Muñoz <munozmunozjavier@gmail.com>
2017-12-04 18:11:25 +0100

    Adding Rx and Threads

---
+++ domain/src/main/java/com/mun0n/domain/executor/ThreadExecutor.java
@@ -0,0 +1,6 @@
+package com.mun0n.domain.executor;
+
+import java.util.concurrent.Executor;
+
+public interface ThreadExecutor extends Executor {
+}
--- domain/src/main/java/com/mun0n/domain/interactor/UseCase.java
+++ domain/src/main/java/com/mun0n/domain/interactor/UseCase.java
@@ -1,4 +1,36 @@
 package com.mun0n.domain.interactor;
 
+import com.mun0n.domain.executor.PostExecutionThread;
+import com.mun0n.domain.executor.ThreadExecutor;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
 public abstract class UseCase<T, Params> {
+    
+    private ThreadExecutor threadExecutor;
+    private final PostExecutionThread postExecutionThread;
+    private CompositeDisposable disposables;
+    
+    public UseCase(final ThreadExecutor threadExecutor,
+                   final PostExecutionThread postExecutionThread) {
+        this.threadExecutor = threadExecutor;
+        this.postExecutionThread = postExecutionThread;
+        disposables = new CompositeDisposable();
+    }
+    
+    abstract Observable<T> buildUseCaseObservable(Params params);
+    
+    public void execute(DisposableObserver<T> observer, Params params) {
+        if (observer != null) {
+            final Observable<T> observable = this.buildUseCaseObservable(params).subscribeOn(
+                    Schedulers.from(threadExecutor)).observeOn(postExecutionThread.getScheduler());
+            addDisposable(observable.subscribeWith(observer));
+        }
+    }
+    
+    private void addDisposable(final DisposableObserver<T> observer) {
+    }
 }
